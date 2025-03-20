d5ed1aa1e521450551769ca4dc41748648c08fb3
Siberian <vasileios.kritikos@aueb.gr>
2025-03-20 10:02:41 +0200

    array demo

---
+++ ArrayCopyDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ArrayCopyDemo;
+/// <summary>
+/// 
+/// </summary>
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] source = {1,2,3,4,5};
+        int[] destination = new int[source.Length];
+        int length = source.Length;
+        
+        Array.Copy(source, 1 , source , 0 , length-1);
+
+        foreach (int el in source)
+        {
+            Console.Write(el + ", ");
+        }
+        Console.WriteLine("----");
+        foreach (int el in destination)
+        {
+            Console.Write(el + ", ");
+        }
+
+    }
+}
